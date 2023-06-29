842736642e51ce5585cc1b84bf88fe0f20fd175b
julinal <137565544+julinal@users.noreply.github.com>
2023-06-28 20:56:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -30,18 +30,4 @@ submitBtn.addEventListener("click", function () {
   result.textContent = "Congratulations, your score is " + score + "!";
 });
 
-//servers
-
-const express = require("express");
-const app = express();
-app.use(express.static("./public"));
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-app.post("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is running on port ${process.env.PORT}.");
-});
 
